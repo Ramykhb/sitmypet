5dae9560fbbac863d25f7ee68bdd64fcf2fa16b3
Ramy Khachab <ramykhb18@gmail.com>
2025-12-28 19:49:05 +0200

    Icon and screen fix

--- frontend/app/(auth)/homeAuth.tsx
+++ frontend/app/(auth)/homeAuth.tsx
@@ -28,14 +28,20 @@ const HomeAuth = () => {
                 <Text className="text-[#0A0A0A] text-lg mt-5 font-bold">Continue as</Text>
                 <TouchableOpacity
                     className="w-[75%] bg-[#3944D5] h-16 rounded-full flex flex-row items-center justify-center my-5"
-                    onPress={() => router.push("/(tabs)")}
+                    onPress={async () => {
+                        await SecureStore.setItemAsync('role', 'owner')
+                        router.push("/(tabs)")
+                    }}
                 >
                     <Image source={require('../../assets/icons/house-filled.png')} alt="logo" className="w-6 h-6 mr-3" />
                     <Text className="text-white text-lg font-bold">Pet Owner</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     className="w-[75%] bg-[#0F1998] h-16 rounded-full flex flex-row items-center justify-center"
-                    onPress={() => router.push("/(tabs)")}
+                    onPress={async () => {
+                        await SecureStore.setItemAsync('role', 'sitter')
+                        router.push("/(tabs)")
+                    }}
                 >
                     <Image source={require('../../assets/icons/paw.png')} alt="logo" className="w-6 h-6 mr-3" />
                     <Text className="text-white text-lg font-bold">Pet Sitter</Text>
--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,10 +2,11 @@ import {Stack, Tabs} from "expo-router";
 import {BlurView} from "expo-blur";
 import "../global.css"
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import {Image, StyleSheet, Text, View, Animated, Dimensions} from "react-native";
-import { useRef, useState } from "react";
+import {Image, StyleSheet, Text, View, Animated, Dimensions, Role} from "react-native";
+import {use, useEffect, useRef, useState} from "react";
+import * as SecureStore from "expo-secure-store";
 
-const TabIcon = ({ focused, icon, title }: any) => {
+const TabIcon = ({focused, icon, title}: any) => {
     return (
         <View className="flex-1 min-w-[85px] w-full min-h-[58px] mt-[31px] justify-center items-center flex-col">
             <Image
@@ -24,12 +25,21 @@ const TabIcon = ({ focused, icon, title }: any) => {
 
 export default function TabsLayout() {
     const indicatorX = useRef(new Animated.Value(0)).current;
+    const [role, setRole] = useState("");
     const [barWidth, setBarWidth] = useState(0);
     const TAB_COUNT = 4;
     const BAR_PADDING = 5;
     const PILL_WIDTH = 85;
     const contentWidth = barWidth - BAR_PADDING * 2;
     const step = contentWidth / TAB_COUNT;
+
+    useEffect(() => {
+        const getRole = async () => {
+            const roletemp = await SecureStore.getItemAsync("role");
+            setRole(roletemp as string);
+        }
+        getRole();
+    }, []);
 
     return <SafeAreaProvider>
         <Tabs
@@ -75,7 +85,7 @@ export default function TabsLayout() {
                                 width: PILL_WIDTH,
                                 borderRadius: 29,
                                 backgroundColor: "rgba(160,160,160,0.2)",
-                                transform: [{ translateX: indicatorX }],
+                                transform: [{translateX: indicatorX}],
                             }}
                         />
                         <View
@@ -88,46 +98,73 @@ export default function TabsLayout() {
                 )
             }}
         >
-            <Tabs.Screen name="index" options={{title:"Home", headerShown: false,  tabBarIcon: ( { focused } ) => (
-                    <TabIcon focused={focused} icon={focused ? require("../../assets/icons/house-filled.png") : require("../../assets/icons/house.png")} title="Home" />
-                )}} listeners={{
-                    focus: () => {
-                        Animated.spring(indicatorX, {
-                            toValue: step * 0,
-                            useNativeDriver: true,
-                        }).start();
-                    },
-                }} />
-            <Tabs.Screen name="index2" options={{title:"Home", headerShown: false,  tabBarIcon: ( { focused } ) => (
-                    <TabIcon focused={focused} icon={require("../../assets/icons/post.png")} title="Post" />
-                )}} listeners={{
-                    focus: () => {
-                        Animated.spring(indicatorX, {
-                            toValue: step * 1,
-                            useNativeDriver: true,
-                        }).start();
-                    },
-                }} />
-            <Tabs.Screen name="index3" options={{title:"Home", headerShown: false,  tabBarIcon: ( { focused } ) => (
-                    <TabIcon focused={focused} icon={focused ? require("../../assets/icons/tray-filled.png") : require("../../assets/icons/tray.png")} title="Requests" />
-                )}} listeners={{
-                    focus: () => {
-                        Animated.spring(indicatorX, {
-                            toValue: step * 2 - 1,
-                            useNativeDriver: true,
-                        }).start();
-                    },
-                }} />
-            <Tabs.Screen name="index4" options={{title:"Home", headerShown: false,  tabBarIcon: ( { focused } ) => (
-                    <TabIcon focused={focused} icon={focused ? require("../../assets/icons/person-filled.png") : require("../../assets/icons/profile.png")} title="Profile" />
-                )}} listeners={{
-                    focus: () => {
-                        Animated.spring(indicatorX, {
-                            toValue: step * 3 - 3,
-                            useNativeDriver: true,
-                        }).start();
-                    },
-                }} />
+            <Tabs.Screen name="index" options={{
+                title: "Home", headerShown: false, tabBarIcon: ({focused}) => (
+                    <TabIcon focused={focused}
+                             icon={focused ? require("../../assets/icons/house-filled.png") : require("../../assets/icons/house.png")}
+                             title="Home"/>
+                )
+            }} listeners={{
+                focus: () => {
+                    Animated.spring(indicatorX, {
+                        toValue: step * 0,
+                        useNativeDriver: true,
+                    }).start();
+                },
+            }}/>
+            <Tabs.Screen name="index2" options={{
+                title: "Home", headerShown: false, tabBarIcon: ({focused}) => (
+                    <TabIcon focused={focused} icon={require("../../assets/icons/post.png")} title="Post"/>
+                )
+            }} listeners={{
+                focus: () => {
+                    Animated.spring(indicatorX, {
+                        toValue: step * 1,
+                        useNativeDriver: true,
+                    }).start();
+                },
+            }}/>
+            {role === 'owner' ? <Tabs.Screen name="index3" options={{
+                title: "Home", headerShown: false, tabBarIcon: ({focused}) => (
+                    <TabIcon focused={focused}
+                             icon={focused ? require("../../assets/icons/tray-filled.png") : require("../../assets/icons/tray.png")}
+                             title="Requests"/>
+                )
+            }} listeners={{
+                focus: () => {
+                    Animated.spring(indicatorX, {
+                        toValue: step * 2 - 1,
+                        useNativeDriver: true,
+                    }).start();
+                },
+            }}/> : <Tabs.Screen name="index3" options={{
+                title: "Home", headerShown: false, tabBarIcon: ({focused}) => (
+                    <TabIcon focused={focused}
+                             icon={focused ? require("../../assets/icons/bookmark-filled.png") : require("../../assets/icons/bookmark.png")}
+                             title="Saved"/>
+                )
+            }} listeners={{
+                focus: () => {
+                    Animated.spring(indicatorX, {
+                        toValue: step * 2 - 1,
+                        useNativeDriver: true,
+                    }).start();
+                },
+            }}/>}
+            <Tabs.Screen name="index4" options={{
+                title: "Home", headerShown: false, tabBarIcon: ({focused}) => (
+                    <TabIcon focused={focused}
+                             icon={focused ? require("../../assets/icons/person-filled.png") : require("../../assets/icons/profile.png")}
+                             title="Profile"/>
+                )
+            }} listeners={{
+                focus: () => {
+                    Animated.spring(indicatorX, {
+                        toValue: step * 3 - 3,
+                        useNativeDriver: true,
+                    }).start();
+                },
+            }}/>
 
         </Tabs>
     </SafeAreaProvider>
